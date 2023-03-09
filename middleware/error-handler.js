2e36503f22c59f0ee5ccc119b5c44cbda928672a
roselagon-dev <pierre.maccini@roselagon.com>
2023-03-09 15:47:59 +0100

    new final project

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -14,19 +14,19 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     customError.msg = Object.values(err.errors)
       .map((item) => item.message)
       .join(',')
-    customError.statusCode = StatusCodes.BAD_REQUEST
+    customError.statusCode = 400
   }
   if (err.code && err.code === 11000) {
     customError.msg = `Duplicate value entered for ${Object.keys(
       err.keyValue
     )} field, please choose another value`
-    customError.statusCode = StatusCodes.BAD_REQUEST
+    customError.statusCode = 400
   }
   if (err.name === 'CastError') {
     customError.msg = `No item found with id : ${err.value}`
-    customError.statusCode = StatusCodes.NOT_FOUND
+    customError.statusCode = 404
   }
-  //return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({err})
+
   return res.status(customError.statusCode).json({ msg: customError.msg })
 }
 
--- routes/jobs.js
+++ routes/jobs.js
@@ -1,9 +1,16 @@
 const express = require('express')
+
 const router = express.Router()
+const {
+  createJob,
+  deleteJob,
+  getAllJobs,
+  updateJob,
+  getJob,
+} = require('../controllers/jobs')
 
-const {getAlljobs, getJob, createJob, updateJob, deleteJob} = require ('../controllers/jobs')
+router.route('/').post(createJob).get(getAllJobs)
 
-router.route('/').post(createJob).get(getAlljobs)
 router.route('/:id').get(getJob).delete(deleteJob).patch(updateJob)
 
 module.exports = router
